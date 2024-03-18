0ecf69d30520a6a75f699eec14bcc36de8d6040f
Filipe Plucenio <plucenio@gmail.com>
2024-03-18 13:21:13 -0300

    Created the methods sign-up and log-in.

--- purple_cloud/cloud/user.js
+++ purple_cloud/cloud/user.js
@@ -1,16 +1,5 @@
 const User = Parse.Object.extend("_User");
 
-Parse.Cloud.define("list-users", async (request) => {
-    const query = new Parse.Query(User);
-    return (await query.find({ useMasterKey: true })).map(function (u) {
-        u = u.toJSON();
-        return {
-            username: u.username,
-            email: u.email
-        };
-    });
-});
-
 Parse.Cloud.define("get-user", async (request) => {
     if (request.params.username != null) {
         const query = new Parse.Query(User);
@@ -20,4 +9,22 @@ Parse.Cloud.define("get-user", async (request) => {
     else {
         throw new Parse.Error(400, "Missing username or password");
     }
+});
+
+Parse.Cloud.define("sign-up", async (request) => {
+    if (request.params.username == null) throw new Parse.Error(400, "Missing username");
+    if (request.params.password == null) throw new Parse.Error(400, "Missing password");
+    const user = new User();
+    user.set("username", request.params.username);
+    user.set("password", request.params.password);
+    return (await user.signUp(null, { useMasterKey: true }));
+});
+
+Parse.Cloud.define("log-in", async (request) => {
+    if (request.params.username == null) throw new Parse.Error(400, "Missing username");
+    if (request.params.password == null) throw new Parse.Error(400, "Missing password");
+    const user = new User();
+    user.set("username", request.params.username);
+    user.set("password", request.params.password);
+    return (await user.logIn(request.params.username, request.params.password));
 });
